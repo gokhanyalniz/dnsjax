f0e39e1875949100c49b074eb751e58777ee849c
Gökhan Yalnız <101704+gokhanyalniz@users.noreply.github.com>
2025-12-19 17:05:53 +0100

    Implement rhs

--- rhs.py
+++ rhs.py
@@ -1,15 +1,35 @@
 import numpy as np
 
-from parameters import *
-import fft
+from fft import FORCE, INV_LAPL, KVEC, LAPL, sx2k
+from parameters import ICF, ISYM, JSYM, NSYM, RE
 
-def rhs_nonlin_term(vel_vfieldxx, vel_vfieldk):
 
-    def rhs_vfieldxx(n):
-        return vel_vfieldxx[ISYM[n]] * vel_vfieldxx[JSYM[n]]
+def rhs_nonlin_term(vel_vfieldx):
+    def rhs_vfieldx(n):
+        return vel_vfieldx[ISYM[n]] * vel_vfieldx[JSYM[n]]
 
-    def rhs_vfieldkk(n):
-        return fft.sx2k(rhs_vfieldxx(n))
+    def rhs_vfieldk(n):
+        return sx2k(rhs_vfieldx(n))
 
-    fvel_vfieldk = None
+    advect = [
+        -sum([1j * KVEC[n] * rhs_vfieldk[NSYM[n, j]] for n in range(3)])
+        for j in range(3)
+    ]
+    div = sum([KVEC[n] * advect[n] for n in range(3)])
+
+    fvel_vfieldk = np.array(
+        [
+            advect[n] + div * INV_LAPL * KVEC[n] + FORCE
+            if n == ICF
+            else advect[n] + div * INV_LAPL * KVEC[n]
+            for n in range(3)
+        ]
+    )
+
+    return fvel_vfieldk
+
+
+def rhs_all(vel_vfieldx, vel_vfieldk):
+    fvel_vfieldk = rhs_nonlin_term(vel_vfieldx) + (LAPL / RE) * vel_vfieldk
+
     return fvel_vfieldk
