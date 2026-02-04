49f2e621046e42dd7210c85b56818f6175c4bfef
Gökhan Yalnız <101704+gokhanyalniz@users.noreply.github.com>
2026-02-04 16:10:38 +0000

    Refactored out the sharding

---
+++ sharding.py
@@ -0,0 +1,24 @@
+import jax
+from jax.sharding import NamedSharding
+from jax.sharding import PartitionSpec as P
+
+# Parallelization
+# Use [1, N] for slab decomposition
+PDIMS = [2, 2]  # TODO: read manually later
+
+# TODO: Check if I can choose the axes to shard
+# - jaxdecomp.autotune for optimal meshing
+
+jax.config.update("jax_enable_x64", True)
+jax.distributed.initialize()
+RANK = jax.process_index()
+
+MESH = jax.make_mesh(PDIMS, axis_names=("Z", "X"))
+SHARDING = NamedSharding(MESH, P("Z", "X"))
+
+# X shape: (ny, nz, nx)
+# Y shape: (nz, nx, ny)
+
+# dnsbox
+# vfieldk(nx_perproc, ny_half, nz, 3)
+# vfieldxx(nyy, nzz_perproc, nxx, 3)
